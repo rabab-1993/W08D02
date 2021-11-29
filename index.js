240afe596c724fb2a7477fbee0c4665255698e4c
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-11-29 18:40:57 +0300

    setup the DB file

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv"); 
-dotenv.config;
+dotenv.config();
+
+const mongoose = require("mongoose");
 const app = express()
 
 
 
 app.use(express.json())
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
+
+
+// imported the db file
+require("./db/index")
+
+
 
 
 
