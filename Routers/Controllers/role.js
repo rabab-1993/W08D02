6959a1a9f690dadc0ae1087607e2d9dcb8e4b975
rabab-1993 <rabab.abdulaziz.elq.js@tuwaiq.edu.sa>
2021-11-29 21:12:52 +0300

    update the Controllers role file

--- Routers/Controllers/role.js
+++ Routers/Controllers/role.js
@@ -1,8 +1,8 @@
 const rolModel = require("../../db/model/role");
-
+// creat new role function
 const creatRole = (req ,res) => {
     const { role, permissions } = req.body;
-    const newRole = new roleModel({
+    const newRole = new rolModel({
       role,
       permissions,
     });
@@ -15,12 +15,21 @@ const creatRole = (req ,res) => {
     .catch((err) => {
       res.status(400).json(err);
     });
+};
+
+// get all role function
+const allRole = (req, res) => {
+    rolModel
+    .find({})
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
 }
 
 
 
 
-
-
-
-module.exports = { creatRole };
+module.exports = { creatRole, allRole };
